402fc4c1396134a4af570c4158ced2a658b19ade
Cameron Lockey <cameron@fragmentlabs.com>
2012-12-08 10:09:20 -0500

    Initial Commit

---
+++ Bandit4e/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bandit4e
+//
+//  Created by Cameron Lockey on 12/8/12.
+//  Copyright (c) 2012 Fragment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
